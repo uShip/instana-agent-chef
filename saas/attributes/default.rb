ad733283c3f52f69168cd88d6c5b7fcc4e9a0361
doerteDev <stefan.staudenmeyer@instana.com>
2016-07-08 10:30:54 +0200

    New dir
    Signed-off-by: doerteDev <stefan.staudenmeyer@instana.com>

--- saas/attributes/default.rb
+++ saas/attributes/default.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 # instana-agent - Attributes file - defining attributes for the instana-agent
-node.default['instana']['tenant']['name'] = 'Your tenant name'
-node.default['instana']['tenant']['unit'] = 'Your tenant unit name'
-node.default['instana']['tenant']['email'] = 'your@email.address'
-node.default['instana']['tenant']['password'] = 'Y0urP4ssw0rd'
+node.default['instana']['tenant']['name'] = 'chegg'
+node.default['instana']['tenant']['unit'] = 'chegg'
+node.default['instana']['tenant']['email'] = 'stan@instana.cpm'
+node.default['instana']['tenant']['password'] = 'ct42devy'
 node.default['instana']['tenant']['agentSrc'] = '/tmp/instana-agent.tgz'
 node.default['instana']['tenant']['agentDest'] = '/opt/instana-agent'
 # node.default['javaloc'] = '/opt/jdk1.8path'
