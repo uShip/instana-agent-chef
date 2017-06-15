daee03baea00b86a2435b4e87d8174dd501f0437
Stefan Staudenmeyer <stefan.staudenmeyer@instana.com>
2017-06-15 11:17:58 +0200

    When 443 is default, y not pick us endpoint as well

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,9 @@
 # instana-agent - Attributes file - defining attributes for the instana-agent
 node.default['instana']['agent']['flavor'] = 'full'
 node.default['instana']['agent']['agent_key'] = ''
-node.default['instana']['agent']['endpoint']['host'] = ''
+node.default['instana']['agent']['endpoint']['host'] = 'saas-us-west-2.instana.io'
 node.default['instana']['agent']['endpoint']['port'] = 443
+node.default['instana']['agent']['jdk'] = ''
 node.default['instana']['agent']['update']['interval'] = 'DAY' # see template agent-update.erb for intervals
 node.default['instana']['agent']['update']['enabled'] = true
 node.default['instana']['agent']['update']['time'] = '04:30'
-node.default['instana']['agent']['jdk'] = ''
