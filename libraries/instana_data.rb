8dc0dccccd01b5ec2cc82cb92a880c7b0ce2d15e
Zachary Schneider <zachary.schneider@instana.com>
2017-08-23 14:52:41 -0500

    sadly not a great idea to mixin, via attributes

--- libraries/instana_data.rb
+++ libraries/instana_data.rb
@@ -1,13 +1,13 @@
-class Chef
-	module DSL::DataQuery
-		def instana_data(field, item, databag="instana-agent")
-			return data_bag_item(databag, item)[field] if item_in_data_bag?(databag, item)
+class Instana
+	def self.data(field, item, databag="instana-agent")
+		return data_bag_item(databag, item)[field] if item_in_data_bag?(databag, item)
 
-			nil
-		end
-
-		def item_in_data_bag?(databag, item)
-      Chef::DataBag.list.include?(databag) && data_bag(databag).include?(item)
-    end
+		nil
 	end
+
+	private
+
+	def self.item_in_data_bag?(databag, item)
+  	Chef::DataBag.list.include?(databag) && data_bag(databag).include?(item)
+  end
 end
