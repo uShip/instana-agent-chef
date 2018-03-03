1f3cb84440faa43ee0ba207b3cb1ff783bdd0141
Marcel Birkner <marcel.birkner@gmail.com>
2018-03-03 12:27:30 +0100

    fix travis build - chef_version
    /home/travis/build/instana/instana-agent-cookbook/metadata.rb:8:in `from_file': undefined method `chef_version' for #<Chef::Cookbook::Metadata:0x00000003965d68> (NoMethodError)

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ license 'Apache-2.0'
 description 'Installs/Configures instana-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.5'
-chef_version '>= 12.20.3'
 
 provides 'instana-agent::backend_config'
 provides 'instana-agent::default'
