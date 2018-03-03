5fa6b3cf684763f8ca59e2e6553d80a24405e9f5
Marcel Birkner <marcel.birkner@gmail.com>
2018-03-03 12:22:13 +0100

    fix travis build
    /home/travis/build/instana/instana-agent-cookbook/metadata.rb:2:in `from_file': undefined method `issues_url' for #<Chef::Cookbook::Metadata:0x0000000230ed30> (NoMethodError)

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,4 @@
 name 'instana-agent'
-issues_url 'https://github.com/instana/instana-agent-cookbook/issues'
-source_url 'https://github.com/instana/instana-agent-cookbook'
 maintainer 'Instana Inc.'
 maintainer_email 'ops@instana.com'
 license 'Apache-2.0'
