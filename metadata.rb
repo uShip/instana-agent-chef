ea9600c757150624cb6c7491480296a518701db0
marcel-birkner <marcel.birkner@codecentric.de>
2018-03-05 14:26:11 +0100

    added source url and issues url

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'ops@instana.com'
 license 'Apache-2.0'
 description 'Installs/Configures instana-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.7'
+version '0.0.8'
+source_url 'https://github.com/instana/instana-agent-cookbook' if defined?(source_url)
+issues_url 'https://github.com/instana/instana-agent-cookbook/issues' if defined?(issues_url)
 
 provides 'instana-agent::backend_config'
 provides 'instana-agent::default'
